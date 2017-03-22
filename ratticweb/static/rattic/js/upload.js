d519eaf71a5539edb101570641b0b5169a7db6b5
Gustavo Parreira <gustavo.parreira@pt.softinsa.com>
2017-03-22 11:16:50 +0000

    Fixed last commit.

--- ratticweb/static/rattic/js/upload.js
+++ ratticweb/static/rattic/js/upload.js
@@ -1,17 +1,11 @@
 var $input = $("#input-0");
-var $uploaded_notice = $("#uploaded-notice")
+var $uploaded_notice = $("#uploaded-notice");
+var $remove_anchor = $("#delete-cookie-anchor");
 var rsa_key = Cookies.get('rsa_key');
-console.log(rsa_key);
-var csrftoken = Cookies.get('csrftoken');
 
 if (rsa_key == null) {
 	$input.show();
 	$input.fileinput({
-		uploadExtraData: {
-			csrfmiddlewaretoken: csrftoken,
-			returnUrl: window.location.href},
-		uploadUrl: "/account/upload/",
-		uploadAsync: false,
 		showUpload: false,
 		showRemove: false,
 		showPreview: false,
@@ -19,13 +13,28 @@ if (rsa_key == null) {
 		minFileCount: 1,
 		maxFileCount: 1,
 	}).on("filebatchselected", function(event, files) {
-		$input.fileinput("upload");
-	});
+		var reader = new FileReader();
 
-	$input.on('filebatchuploadsuccess', function(event, data, previewId, index) {
-		console.log(data.extra.returnUrl);
-		window.location.href = data.extra.returnUrl;
+		reader.onload = function (e) {
+			saveCookie(e.target.result);
+			window.location.href = window.location.href;
+		}
+		reader.readAsText(files[0]);
+
 	});
 } else {
-	$uploaded_notice.show()
+	$uploaded_notice.show();
+	$remove_anchor.click(function() {
+		deleteCookie();
+		window.location.href = window.location.href;
+	});
+}
+
+function saveCookie(key) {
+	var inThreeHours = new Date(new Date().getTime() + 3 * 60 * 60 * 1000);
+	Cookies.set('rsa_key', key, { expires: inThreeHours });
+}
+
+function deleteCookie() {
+	Cookies.remove('rsa_key')
 }

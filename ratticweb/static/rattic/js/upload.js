f9bd9cc277999505bf8ee42de28a64790202c596
Gustavo Parreira <gustavo.parreira@pt.softinsa.com>
2017-03-22 12:29:44 +0000

    Implemented Local Storage if Web Storage is available in current browser, instead of cookies. Started working on encrypting and decrypting client side if the password is private.

--- ratticweb/static/rattic/js/upload.js
+++ ratticweb/static/rattic/js/upload.js
@@ -1,7 +1,20 @@
 var $input = $("#input-0");
 var $uploaded_notice = $("#uploaded-notice");
 var $remove_anchor = $("#delete-cookie-anchor");
-var rsa_key = Cookies.get('rsa_key');
+var localStorageAvailable = false;
+
+if (typeof(Storage) !== "undefined") {
+	console.log("Web Storage available.");
+	localStorageAvailable = true;
+}
+
+if (localStorageAvailable) {
+	var rsa_key = getKeyValue();
+} else {
+	var rsa_key = Cookies.get('rsa_key');
+}
+
+checkIfKeyIsExpired();
 
 if (rsa_key == null) {
 	$input.show();
@@ -16,7 +29,11 @@ if (rsa_key == null) {
 		var reader = new FileReader();
 
 		reader.onload = function (e) {
-			saveCookie(e.target.result);
+			if (localStorageAvailable) {
+				saveKey(e.target.result);
+			} else {
+				saveCookie(e.target.result);
+			}
 			window.location.href = window.location.href;
 		}
 		reader.readAsText(files[0]);
@@ -25,7 +42,11 @@ if (rsa_key == null) {
 } else {
 	$uploaded_notice.show();
 	$remove_anchor.click(function() {
-		deleteCookie();
+		if (localStorageAvailable) {
+			deleteKey();
+		} else {
+			deleteCookie();
+		}
 		window.location.href = window.location.href;
 	});
 }
@@ -37,4 +58,36 @@ function saveCookie(key) {
 
 function deleteCookie() {
 	Cookies.remove('rsa_key')
+}
+
+function saveKey(key) {
+	var inThreeHours = new Date(new Date().getTime() + 3 * 60 * 60 * 1000);
+	var keyToStore = {
+		value: key,
+		expires: inThreeHours
+	};
+	localStorage.setItem('rsa_key', JSON.stringify(keyToStore));
+}
+
+function getKeyValue() {
+	if (localStorage.getItem('rsa_key') !== null) {
+		var object = JSON.parse(localStorage.rsa_key);
+		return object.value;
+	};
+	return null;
+}
+
+function checkIfKeyIsExpired() {
+	if (localStorage.getItem('rsa_key') !== null) {
+		var object = JSON.parse(localStorage.rsa_key);
+		var date = object.expires;
+
+		if (date > (new Date().getTime())) {
+			deleteKey();
+		};
+	};
+}
+
+function deleteKey() {
+	localStorage.removeItem('rsa_key');
 }

0fca141a2ee71d1b8607d16946c0394005a31b68
Gustavo Parreira <gustavo.parreira@pt.softinsa.com>
2017-03-24 16:10:34 +0000

    No longer showing private groups in new request list.

--- request/forms.py
+++ request/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm, Select
 from cred.models import CredTemp, Cred
 from django.db.models import Q
+from django.contrib.auth.models import User, Group
 from django import forms
 import logging
 
@@ -12,7 +13,10 @@ class CredTempForm(ModelForm):
 		self.fields['cred'].label = 'Password'
 		logger.info("requser = " + str(requser))
 		existing_temp_creds = CredTemp.objects.filter(user=requser).values_list('cred_id', flat=True)
-		available_creds = Cred.objects.filter(Q(latest=None) & Q(is_deleted=False)).exclude(Q(id__in=existing_temp_creds))
+		self_groups = [user.self_group_id for user in User.objects.all()]
+		groups_user_is_in = [group.id for group in requser.groups.all()]
+		available_creds = Cred.objects.filter(Q(latest=None) & Q(is_deleted=False)).exclude(Q(id__in=existing_temp_creds) | Q(group_id__in=self_groups) | Q(group_id__in=groups_user_is_in))
+		logger.info(str([cred.group for cred in available_creds]))
                 
 		self.fields['cred'].queryset = available_creds
 
